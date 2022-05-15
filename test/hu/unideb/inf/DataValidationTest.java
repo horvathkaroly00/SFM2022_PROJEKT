88808809f70ff36d30ecc21d776cba369c1287d2
univerzalisbalhe <ilyesdavid2@gmail.com>
2022-05-15 18:54:58 +0200

    test
    test

--- test/hu/unideb/inf/DataValidationTest.java
+++ test/hu/unideb/inf/DataValidationTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class DataValidationTest {
+public class DataValidationTest {
 
     @Test
-    void usernameFormat() {
+    void usernameFormatcorrect() {
+        var user = new DataValidationTest();
+
     }
 
     @Test
--- test/hu/unideb/inf/GuiControllerTest.java
+++ test/hu/unideb/inf/GuiControllerTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class GuiControllerTest {
+public class GuiControllerTest {
 
     @Test
     void switchToRegisterWindow() {
