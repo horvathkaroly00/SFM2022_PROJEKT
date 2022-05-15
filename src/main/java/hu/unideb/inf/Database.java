da7c7cab8de6c341ab505bd3f3a71b9e55296ae3
horvathkaroly00 <karika7072@gmail.com>
2022-05-15 19:46:23 +0200

    update!!!!

--- src/main/java/hu/unideb/inf/Database.java
+++ src/main/java/hu/unideb/inf/Database.java
@@ -6,6 +6,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
 
 public class Database {
 
@@ -14,7 +15,8 @@ public class Database {
     private static final String DATABASE_USERNAME = "Kdr123";
     private static final String DATABASE_PASSWORD = "Kdrteam123";
     private static final String SELECT_QUERY = "SELECT * FROM users WHERE username = ? and userpassword = ?";
-    private static final String INSERT_QUERY = "INSERT INTO users (username,userpassword,email,teljesnev,szuldatum,szemelyigazolvany,lakcimkartya,tajkartya ) VALUES (?, ?, ?, ?, ?,?, ?, ?)";
+    private static final String INSERT_QUERY = "INSERT INTO users (username,email,userpassword,teljesnev,szuldatum,szemelyigazolvany,lakcimkartya,tajkartya ) VALUES (?, ?, ?, ?, ?,?, ?, ?)";
+    private static final String INSERT_QUERY_UGYEK = "INSERT INTO ugyek (teljesnev, ugy, sorszam, idopont) VALUES (?, ?, ?, ?)";
 
     public boolean validate(String username, String userpassword) throws SQLException {
 
@@ -44,7 +46,7 @@ public class Database {
         return false;
     }
 
-    public void insertRecord(String username, String email, String userpassword,String teljesnev, String szuldatum, String szemelyigazolvany,String lakcimkartya,String tajkartya) throws SQLException {
+    public void insertRecord(String username, String userpassword, String email, String teljesnev, String szuldatum, String szemelyigazolvany,String lakcimkartya,String tajkartya) throws SQLException {
 
         // Step 1: Establishing a Connection and
         // try-with-resource statement will auto close the connection.
@@ -54,8 +56,8 @@ public class Database {
              // Step 2:Create a statement using connection object
              PreparedStatement preparedStatement = connection.prepareStatement(INSERT_QUERY)) {
             preparedStatement.setString(1, username);
-            preparedStatement.setString(2, userpassword);
-            preparedStatement.setString(3, email);
+            preparedStatement.setString(2, email);
+            preparedStatement.setString(3, userpassword);
             preparedStatement.setString(4, teljesnev);
             preparedStatement.setString(5, szuldatum);
             preparedStatement.setString(6, szemelyigazolvany);
@@ -70,6 +72,31 @@ public class Database {
             printSQLException(e);
         }
     }
+
+    public void insertRecordUgyek(String teljesnev, String ugy, String sorszam, String idopont){
+        // Step 1: Establishing a Connection and
+        // try-with-resource statement will auto close the connection.
+        try (Connection connection = DriverManager
+                .getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
+
+             // Step 2:Create a statement using connection object
+             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_QUERY_UGYEK)) {
+            preparedStatement.setString(1, teljesnev);
+            preparedStatement.setString(2, ugy);
+            preparedStatement.setString(3, sorszam);
+            preparedStatement.setString(4, idopont);
+
+            System.out.println(preparedStatement);
+            // Step 3: Execute the query or update query
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            // print SQL exception information
+            printSQLException(e);
+
+        }
+    }
+
+    
 
     public static void printSQLException(SQLException ex) {
         for (Throwable e: ex) {
--- src/main/java/hu/unideb/inf/GuiController.java
+++ src/main/java/hu/unideb/inf/GuiController.java
@@ -17,12 +17,16 @@ import javafx.stage.Window;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Random;
 import java.util.ResourceBundle;
 
 
 
 public class GuiController implements Initializable {
+
+
 
 
     private Stage stage;
@@ -171,6 +175,7 @@ public class GuiController implements Initializable {
     Button egeszsButton, eesztButton, vedettButton, vallButton, cegButton, munkaButton;
     public void display (ActionEvent event)
     {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
         Random rand = new Random();
         int n = rand.nextInt(50);
         n += 1;
@@ -180,79 +185,170 @@ public class GuiController implements Initializable {
             pnl_pult.toFront();
             ugyLabel.setText(diakhitelButton.getText());
             sorszamLabel.setText(Integer.toString(n));
+            String teljesnev = globalusername;
+            String ugy = ugyLabel.getText();
+            String sorszam = sorszamLabel.getText();
+            LocalDateTime idopont = LocalDateTime.now();
+            Database database = new Database();
+            database.insertRecordUgyek(teljesnev, ugy, sorszam, String.valueOf(idopont));
         } else if (event.getSource() == nyelvvButton) {
             hboxLabel.setText("SORSZÁM");
             pnl_pult.toFront();
             ugyLabel.setText(nyelvvButton.getText());
             sorszamLabel.setText(Integer.toString(n));
+            String teljesnev = globalusername;
+            String ugy = ugyLabel.getText();
+            String sorszam = sorszamLabel.getText();
+            LocalDateTime idopont = LocalDateTime.now();
+            Database database = new Database();
+            database.insertRecordUgyek(teljesnev, ugy, sorszam, String.valueOf(idopont));
         } else if (event.getSource() == erettButton) {
             hboxLabel.setText("SORSZÁM");
             pnl_pult.toFront();
             ugyLabel.setText(erettButton.getText());
             sorszamLabel.setText(Integer.toString(n));
+            String teljesnev = globalusername;
+            String ugy = ugyLabel.getText();
+            String sorszam = sorszamLabel.getText();
+            LocalDateTime idopont = LocalDateTime.now();
+            Database database = new Database();
+            database.insertRecordUgyek(teljesnev, ugy, sorszam, String.valueOf(idopont));
         } else if (event.getSource() == jogsiButton) {
             hboxLabel.setText("SORSZÁM");
             pnl_pult.toFront();
             ugyLabel.setText(jogsiButton.getText());
             sorszamLabel.setText(Integer.toString(n));
+            String teljesnev = globalusername;
+            String ugy = ugyLabel.getText();
+            String sorszam = sorszamLabel.getText();
+            LocalDateTime idopont = LocalDateTime.now();
+            Database database = new Database();
+            database.insertRecordUgyek(teljesnev, ugy, sorszam, String.valueOf(idopont));
         } else if (event.getSource() == diakButton) {
             hboxLabel.setText("SORSZÁM");
             pnl_pult.toFront();
             ugyLabel.setText(diakButton.getText());
             sorszamLabel.setText(Integer.toString(n));
+            String teljesnev = globalusername;
+            String ugy = ugyLabel.getText();
+            String sorszam = sorszamLabel.getText();
+            LocalDateTime idopont = LocalDateTime.now();
+            Database database = new Database();
+            database.insertRecordUgyek(teljesnev, ugy, sorszam, String.valueOf(idopont));
         } else if (event.getSource() == szemelyButton) {
             hboxLabel.setText("SORSZÁM");
             pnl_pult.toFront();
             ugyLabel.setText(szemelyButton.getText());
             sorszamLabel.setText(Integer.toString(n));
+            String teljesnev = globalusername;
+            String ugy = ugyLabel.getText();
+            String sorszam = sorszamLabel.getText();
+            LocalDateTime idopont = LocalDateTime.now();
+            Database database = new Database();
+            database.insertRecordUgyek(teljesnev, ugy, sorszam, String.valueOf(idopont));
         } else if (event.getSource() == biztButton) {
             hboxLabel.setText("SORSZÁM");
             pnl_pult.toFront();
             ugyLabel.setText(biztButton.getText());
             sorszamLabel.setText(Integer.toString(n));
+            String teljesnev = globalusername;
+            String ugy = ugyLabel.getText();
+            String sorszam = sorszamLabel.getText();
+            LocalDateTime idopont = LocalDateTime.now();
+            Database database = new Database();
+            database.insertRecordUgyek(teljesnev, ugy, sorszam, String.valueOf(idopont));
         } else if (event.getSource() == atirButton) {
             hboxLabel.setText("SORSZÁM");
             pnl_pult.toFront();
             ugyLabel.setText(atirButton.getText());
             sorszamLabel.setText(Integer.toString(n));
+            String teljesnev = globalusername;
+            String ugy = ugyLabel.getText();
+            String sorszam = sorszamLabel.getText();
+            LocalDateTime idopont = LocalDateTime.now();
+            Database database = new Database();
+            database.insertRecordUgyek(teljesnev, ugy, sorszam, String.valueOf(idopont));
         } else if (event.getSource() == eredetButton) {
             hboxLabel.setText("SORSZÁM");
             pnl_pult.toFront();
             ugyLabel.setText(eredetButton.getText());
             sorszamLabel.setText(Integer.toString(n));
+            String teljesnev = globalusername;
+            String ugy = ugyLabel.getText();
+            String sorszam = sorszamLabel.getText();
+            LocalDateTime idopont = LocalDateTime.now();
+            Database database = new Database();
+            database.insertRecordUgyek(teljesnev, ugy, sorszam, String.valueOf(idopont));
         } else if (event.getSource() == egeszsButton) {
             hboxLabel.setText("SORSZÁM");
             pnl_pult.toFront();
             ugyLabel.setText(egeszsButton.getText());
             sorszamLabel.setText(Integer.toString(n));
+            String teljesnev = globalusername;
+            String ugy = ugyLabel.getText();
+            String sorszam = sorszamLabel.getText();
+            LocalDateTime idopont = LocalDateTime.now();
+            Database database = new Database();
+            database.insertRecordUgyek(teljesnev, ugy, sorszam, String.valueOf(idopont));
         } else if (event.getSource() == eesztButton) {
             hboxLabel.setText("SORSZÁM");
             pnl_pult.toFront();
             ugyLabel.setText(eesztButton.getText());
             sorszamLabel.setText(Integer.toString(n));
+            String teljesnev = globalusername;
+            String ugy = ugyLabel.getText();
+            String sorszam = sorszamLabel.getText();
+            LocalDateTime idopont = LocalDateTime.now();
+            Database database = new Database();
+            database.insertRecordUgyek(teljesnev, ugy, sorszam, String.valueOf(idopont));
         } else if (event.getSource() == vedettButton) {
             hboxLabel.setText("SORSZÁM");
             pnl_pult.toFront();
             ugyLabel.setText(vedettButton.getText());
             sorszamLabel.setText(Integer.toString(n));
+            String teljesnev = globalusername;
+            String ugy = ugyLabel.getText();
+            String sorszam = sorszamLabel.getText();
+            LocalDateTime idopont = LocalDateTime.now();
+            Database database = new Database();
+            database.insertRecordUgyek(teljesnev, ugy, sorszam, String.valueOf(idopont));
         } else if (event.getSource() == vallButton) {
             hboxLabel.setText("SORSZÁM");
             pnl_pult.toFront();
             ugyLabel.setText(vallButton.getText());
             sorszamLabel.setText(Integer.toString(n));
+            String teljesnev = globalusername;
+            String ugy = ugyLabel.getText();
+            String sorszam = sorszamLabel.getText();
+            LocalDateTime idopont = LocalDateTime.now();
+            Database database = new Database();
+            database.insertRecordUgyek(teljesnev, ugy, sorszam, String.valueOf(idopont));
         } else if (event.getSource() == cegButton) {
             hboxLabel.setText("SORSZÁM");
             pnl_pult.toFront();
             ugyLabel.setText(cegButton.getText());
             sorszamLabel.setText(Integer.toString(n));
+            String teljesnev = globalusername;
+            String ugy = ugyLabel.getText();
+            String sorszam = sorszamLabel.getText();
+            LocalDateTime idopont = LocalDateTime.now();
+            Database database = new Database();
+            database.insertRecordUgyek(teljesnev, ugy, sorszam, String.valueOf(idopont));
         } else if (event.getSource() == munkaButton) {
             hboxLabel.setText("SORSZÁM");
             pnl_pult.toFront();
             ugyLabel.setText(munkaButton.getText());
             sorszamLabel.setText(Integer.toString(n));
+            String teljesnev = globalusername;
+            String ugy = ugyLabel.getText();
+            String sorszam = sorszamLabel.getText();
+            LocalDateTime idopont = LocalDateTime.now();
+            Database database = new Database();
+            database.insertRecordUgyek(teljesnev, ugy, sorszam, String.valueOf(idopont));
         }
 
     }
+    public static String globalusername;
     //login
     @FXML
     private TextField usernameField;
@@ -279,87 +375,85 @@ public class GuiController implements Initializable {
             stage.getIcons().add(new Image("/images/kdr.png"));
             ((GuiController) fxmlLoader.getController()).init(stage);
             stage.show();
+
+            globalusername = username;
         }
     }
     //register
     @FXML
     private Label birthdateErrorLabel, emailErrorLabel, fullnameErrorLabel, lakcimkartyaErrorLabel, passwordErrorLabel,  passwordujraErrorLabel;
+    @FXML
     private Label szemelyiErrorLabel, tajErrorLabel, usernameErrorLabel;
     @FXML
     private TextField usernameRegisterField, teljesnevTextField, szemelyiTextField, lakcimTextField, szuldatumTextField, emailTextField, tajTextField,emailUjraTextField;
     @FXML
     private PasswordField passwordRegisterField, passwordUjraRegisterField;
     @FXML
-    public void register(ActionEvent event) throws SQLException {
-
-        Window owner = btn_veglegesit.getScene().getWindow();
-
-        if (usernameRegisterField.getText().isEmpty()) {
-            showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
-                    "id hianyzik");
-            return;
-        }
-        if (emailTextField.getText().isEmpty()) {
-            showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
-                    "email hianyzik");
-            return;
-        }
-        if (emailUjraTextField.getText().isEmpty()) {
-            showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
-                    "ujra email hianyzik");
-            return;
-        }
-        if (passwordRegisterField.getText().isEmpty()) {
-            showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
-                    "jelszo hianyzik");
-            return;
-        }
-        if (passwordUjraRegisterField.getText().isEmpty()) {
-            showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
-                    "ujra jelszo hianyzik");
-            return;
-        }
-        if (teljesnevTextField.getText().isEmpty()) {
-            showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
-                    "teljes nev hianyzik");
-            return;
-        }
-        if (szemelyiTextField.getText().isEmpty()) {
-            showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
-                    "szemelyi hianyzik");
-            return;
-        }
-        if (lakcimTextField.getText().isEmpty()) {
-            showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
-                    "lakcimkartya hianyzik");
-            return;
-        }
-        if (szuldatumTextField.getText().isEmpty()) {
-            showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
-                    "szuletesi datum hianyzik");
-            return;
-        }
-        if (tajTextField.getText().isEmpty()) {
-            showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
-                    "taj hianyzik");
-            return;
-        }
-
-        String username = usernameRegisterField.getText();
-        String userpassword = passwordRegisterField.getText();
-        String email = emailTextField.getText();
-        String teljesnev = teljesnevTextField.getText();
-        String szemelyigazolvany = szemelyiTextField.getText();
-        String lakcimkartya = lakcimTextField.getText();
-        String szuldatum = szuldatumTextField.getText();
-        String tajkartya = tajTextField.getText();
+    public void register(ActionEvent event) throws SQLException, IOException{
 
 
-        Database database = new Database();
-        database.insertRecord(username,userpassword, email, teljesnev,szuldatum, szemelyigazolvany,lakcimkartya,tajkartya);
+        if(!(passwordRegisterField.getText().equals(passwordUjraRegisterField.getText()))){
+            passwordujraErrorLabel.setText("Nem egyezik a két jelszó!");
+        }else {
+            passwordujraErrorLabel.setText("");
+        }
 
-        showAlert(Alert.AlertType.CONFIRMATION, owner, "sikeres",
-                "Welcome " + usernameRegisterField.getText());
+        boolean usernameValidation = DataValidation.usernameFormat(usernameRegisterField, usernameErrorLabel, "Helytelen felhasználónév formátum.Kérem adjon meg minimum 6 karakterből, számokból, és betőkből álló felhasználónevet.");
+        boolean emailValidation = DataValidation.emailFormat(emailTextField, emailErrorLabel, "Helytelen email formátum. Helyes e-mail címre egy példa: ricsi722@gmail.com");
+        boolean passwordValidation = DataValidation.passwordFormat(passwordRegisterField, passwordErrorLabel, "Helytelen jelszó formátum. Tartalmazzon minimum egy kis és nagy betűt,számot,minimum 8 karakter hosszú legyen.");
+        boolean fullnameValidation = DataValidation.fullnameFormat(teljesnevTextField,fullnameErrorLabel,"Kérem adja meg a nevét!");
+        boolean birthdateValidation = DataValidation.birthdateFormat(szuldatumTextField,birthdateErrorLabel,"Kérem kötőjellel elválasztva adja meg születési dátumát!");
+        boolean idcardValidation = DataValidation.idcardFormat(szemelyiTextField,szemelyiErrorLabel,"Kérem ehhez hasonló módon adja meg személyi igazolvány számát:123456AS");
+        boolean addresscardValidation = DataValidation.addresscardFormat(lakcimTextField,lakcimkartyaErrorLabel,"Kérem ehhez hasonló módon adja meg lakcím kártyájának  számát:123456AS");
+        boolean tajcardValidation = DataValidation.tajcardFormat(tajTextField,tajErrorLabel,"Kérem ehhez hasonló módon adja meg TAJ-számát:123456789");
+/*
+        boolean username = DataValidation.textFieldIsNull(usernameRegisterField,usernameErrorLabel, "zID is Required");
+        boolean email = DataValidation.textFieldIsNull(emailTextField, emailErrorLabel, "Name is Required");
+        boolean password = DataValidation.textFieldIsNull(passwordRegisterField,passwordErrorLabel, "Phone Number is Required");
+        boolean passwordagain = DataValidation.textFieldIsNull(passwordUjraRegisterField,passwordujraErrorLabel, "Email is Required");
+        boolean fullname = DataValidation.textFieldIsNull(teljesnevTextField,fullnameErrorLabel, "Course Code is Required");
+        boolean birthdate = DataValidation.textFieldIsNull(szuldatumTextField,birthdateErrorLabel, "Course Code is Required");
+        boolean idcard = DataValidation.textFieldIsNull(szemelyiTextField,szemelyiErrorLabel, "Course Code is Required");
+        boolean addresscard = DataValidation.textFieldIsNull(lakcimTextField,lakcimkartyaErrorLabel, "Course Code is Required");
+        boolean tajcard = DataValidation.textFieldIsNull(tajTextField,tajErrorLabel, "Course Code is Required");
+*/
+        if (usernameValidation && emailValidation && passwordValidation && fullnameValidation && birthdateValidation && idcardValidation && addresscardValidation && tajcardValidation && tajcardValidation == true)
+        {
+            String username = usernameRegisterField.getText();
+            String userpassword = passwordRegisterField.getText();
+            String email = emailTextField.getText();
+            String teljesnev = teljesnevTextField.getText();
+            String szemelyigazolvany = szemelyiTextField.getText();
+            String lakcimkartya = lakcimTextField.getText();
+            String szuldatum = szuldatumTextField.getText();
+            String tajkartya = tajTextField.getText();
+
+
+            Database database = new Database();
+            database.insertRecord(username,userpassword, email, teljesnev,szuldatum, szemelyigazolvany,lakcimkartya,tajkartya);
+
+            Window owner = btn_veglegesit.getScene().getWindow();
+            showAlert(Alert.AlertType.CONFIRMATION, owner, "sikeres",
+                    "Welcome " + usernameRegisterField.getText());
+
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/LoginWindow.fxml"));
+            Parent root = (Parent) fxmlLoader.load();
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.centerOnScreen();
+            stage.getIcons().add(new Image("/images/kdr.png"));
+            ((GuiController) fxmlLoader.getController()).init(stage);
+            stage.show();
+
+        }
+        else {
+            Window owner = btn_veglegesit.getScene().getWindow();
+            showAlert(Alert.AlertType.CONFIRMATION, owner, "szar", "szar ");
+
+        }
+
+
     }
 
     private static void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
@@ -369,6 +463,24 @@ public class GuiController implements Initializable {
         alert.setContentText(message);
         alert.initOwner(owner);
         alert.show();
+    }
+
+
+    @FXML
+    private Label profilUsernameLabel, profilEmailLabel, profilPasswordLabel, profilFullnameLabel, profilSzuldatumLabel, profilSzemelyiLabel,
+            profilLakcimLabel, profilTajLabel;
+    @FXML
+    private Pane pnl_profil;
+    @FXML
+    private  Button btn_profil;
+    public void sajatProfil(ActionEvent event) throws SQLException {
+        if (event.getSource() == btn_profil)
+        {
+            profilUsernameLabel.setText(globalusername);
+            pnl_profil.toFront();
+
+        }
+
     }
 
     @Override
